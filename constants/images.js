99531f92c2097603f8463232828e16b0e11810ee
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-04-30 15:30:45 +0000

    edit images.js

--- constants/images.js
+++ constants/images.js
@@ -1,4 +1,4 @@
-export const otherWordsForHome = require("../assets/images/other_words_for_home,jpg");
+export const otherWordsForHome = require("../assets/images/other_words_for_home.jpg");
 export const theMetropolist = require("../assets/images/the_metropolist.jpg");
 export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
 export const underland = require("../assets/images/underland.jpg");
