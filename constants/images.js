fdca0f103912cb836e4998436345282aed925bd5
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-05-17 11:09:45 +0000

    took out metropolis and new book images

--- constants/images.js
+++ constants/images.js
@@ -1,11 +1,15 @@
 export const otherWordsForHome = require("../assets/images/other_words_for_home.jpg");
-export const theMetropolist = require("../assets/images/the_metropolist.jpg");
+export const ariDante = require("../assets/images/aridante.jpg");
+export const blackTides = require("../assets/images/blacktides.jpg");
+export const oathbringer = require("../assets/images/oathbringer.jpg");
 export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
 export const underland = require("../assets/images/underland.jpg");
 
 export default {
   otherWordsForHome,
-  theMetropolist,
+  ariDante,
+  blackTides,
+  oathbringer,
   theTinyDragon,
   underland,
 };
